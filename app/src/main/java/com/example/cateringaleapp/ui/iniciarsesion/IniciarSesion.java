1405a8f3467c92002d03491bb3ac109bb725dabb
Manuel Angeles De La Cruz <angeles6_359@hotmail.com>
2020-04-05 19:17:40 -0500

    Se añadió validación de datos en activities iniciar sesión y registrar usuario.

--- app/src/main/java/com/example/cateringaleapp/ui/iniciarsesion/IniciarSesion.java
+++ app/src/main/java/com/example/cateringaleapp/ui/iniciarsesion/IniciarSesion.java
@@ -2,6 +2,7 @@ package com.example.cateringaleapp.ui.iniciarsesion;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cateringaleapp.R;
 
 public class IniciarSesion extends Fragment {
 
     private IniciarSesionViewModel mViewModel;
+    private Button btnIngresar;
+    private EditText txtUsuario, txtContraseña;
 
     public static IniciarSesion newInstance() {
         return new IniciarSesion();
@@ -25,7 +32,50 @@ public class IniciarSesion extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.iniciar_sesion_fragment, container, false);
+
+        //return inflater.inflate(R.layout.iniciar_sesion_fragment, container, false);
+        View view = inflater.inflate(R.layout.iniciar_sesion_fragment, container, false);
+
+        btnIngresar =  (Button) view.findViewById(R.id.btnISIngresar);
+        txtUsuario = (EditText) view.findViewById(R.id.txtISUsuario);
+        txtContraseña = (EditText) view.findViewById(R.id.txtISContraseña);
+
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean faltanDatos = false;
+                String usuario = txtUsuario.getText().toString();
+                String contraseña = txtContraseña.getText().toString();
+
+
+                if(usuario.isEmpty()){
+                    faltanDatos = true;
+                    txtUsuario.setError("Dato requerido.");
+                }
+                else{
+                    txtUsuario.setError(null);
+                }
+
+                if(contraseña.isEmpty()){
+                    faltanDatos = true;
+                    txtContraseña.setError("Dato requerido.");
+                }else{
+                    txtContraseña.setError(null);
+                }
+
+
+                if(faltanDatos){
+
+                    Toast.makeText(getContext(),"Favor de completar los datos requeridos.",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+        return view;
     }
 
     @Override
--- app/src/main/java/com/example/cateringaleapp/ui/miperfil/MiPerfilFragment.java
+++ app/src/main/java/com/example/cateringaleapp/ui/miperfil/MiPerfilFragment.java
@@ -11,12 +11,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cateringaleapp.R;
 
 public class MiPerfilFragment extends Fragment {
 
     private MiPerfilViewModel mViewModel;
+
+    Button btnCrearCuenta;
+    EditText txtNombre, txtCorreo,txtContraseña,txtTelefono,txtDireccion;
+
+
 
     public static MiPerfilFragment newInstance() {
         return new MiPerfilFragment();
@@ -25,7 +33,77 @@ public class MiPerfilFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.mi_perfil_fragment, container, false);
+        //return inflater.inflate(R.layout.mi_perfil_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.mi_perfil_fragment, container, false);
+
+        btnCrearCuenta = (Button) view.findViewById(R.id.btnRegistrar);
+        txtNombre = (EditText) view.findViewById(R.id.txtNombre);
+        txtCorreo = (EditText) view.findViewById(R.id.txtCorreo);
+        txtContraseña = (EditText) view.findViewById(R.id.txtContraseña);
+        txtTelefono = (EditText) view.findViewById(R.id.txtTelefono);
+        txtDireccion = (EditText) view.findViewById(R.id.txtDireccion);
+
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                boolean faltanDatos = false;
+
+                String Nombre = txtNombre.getText().toString();
+                String Correo = txtCorreo.getText().toString();
+                String Contraseña = txtContraseña.getText().toString();
+                String Telefono = txtTelefono.getText().toString();
+                String Direccion = txtDireccion.getText().toString();
+
+                if(Nombre.isEmpty()){
+                    faltanDatos = true;
+                    txtNombre.setError("Dato requerido.");
+                }
+                else{
+                    txtNombre.setError(null);
+                }
+
+                if(Correo.isEmpty()){
+                    faltanDatos = true;
+                    txtCorreo.setError("Dato requerido.");
+                }else{
+                    txtCorreo.setError(null);
+                }
+
+                if(Contraseña.isEmpty()){
+                    faltanDatos = true;
+                    txtContraseña.setError("Dato requerido.");
+                }else{
+                    txtContraseña.setError(null);
+                }
+
+                if(Telefono.isEmpty()){
+                    faltanDatos = true;
+                    txtTelefono.setError("Dato requerido.");
+                }else{
+                    txtTelefono.setError(null);
+                }
+
+                if(Direccion.isEmpty()){
+                    faltanDatos = true;
+                    txtDireccion.setError("Dato requerido.");
+                }else{
+                    txtDireccion.setError(null);
+                }
+
+
+
+                if(faltanDatos){
+
+                    Toast.makeText(getContext(),"Favor de completar los datos requeridos.",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        return view;
     }
 
     @Override
