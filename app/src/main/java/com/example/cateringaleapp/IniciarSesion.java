3173a967a619e1f6bf6c8273e4e589b9537d67ab
Manuel Angeles De La Cruz <angeles6_359@hotmail.com>
2020-04-18 10:42:11 -0500

    Cambios en diseños de pantallas.

--- app/src/main/java/com/example/cateringaleapp/IniciarSesion.java
+++ app/src/main/java/com/example/cateringaleapp/IniciarSesion.java
@@ -4,10 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -54,9 +77,12 @@ public class IniciarSesion extends AppCompatActivity {
         if(faltanDatos){
 
             Toast.makeText(this,"Favor de completar los datos requeridos.",Toast.LENGTH_LONG).show();
+
         }else{
+
             Intent anterior = new Intent(this,MainActivity.class);
             startActivity(anterior);
+
         }
 
 
@@ -68,4 +94,7 @@ public class IniciarSesion extends AppCompatActivity {
         startActivity(anterior);
 
     }
+
+
+
 }
